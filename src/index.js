f02aa9585747f1dc96ecdbd9228e83bea7e5338e
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-09-28 17:00:08 +0300

    add static path for css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const initHandlebars = require('./config/handlebars.js');
 
 const app = express();
 
 initHandlebars(app);
+
+app.use(express.static(path.resolve('./src/static')));
 
 app.all('/', (req, res) => {
     res.render('index');
