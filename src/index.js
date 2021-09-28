51bcd1e3f8720fc623ffe12e9b804f5934feadc2
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-09-28 16:51:29 +0300

    add handlebars and layouts/main.hbs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
+
+const initHandlebars = require('./config/handlebars.js');
 
 const app = express();
 
-app.set('views', path.resolve('./src/views'));
-app.engine('hbs', handlebars({
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+initHandlebars(app);
 
 app.all('/', (req, res) => {
-    res.render('index', { layout: false });
+    res.render('index');
 });
 
 app.listen(5000, console.log.bind(console, 'Server is running on http://localhost:5000'));
