29bd90a0f62ef9f9c6f8aadc4ca434659b17922d
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-09-28 16:30:39 +0300

    add handlebars views static config

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.resolve('./src/views'));
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
+app.all('/', (req, res) => {
+    res.render('index', { layout: false });
+});
+
+app.listen(5000, console.log.bind(console, 'Server is running on http://localhost:5000'));
