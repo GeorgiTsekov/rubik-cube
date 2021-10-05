f5eb9c00080dfad1bd7ae4405cad4d03413873b0
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-10-05 10:58:09 +0300

    add accessories select for attach page

--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -4,8 +4,13 @@ async function create(name, imageUrl, description) {
     return Accessory.create({ name, imageUrl, description });
 };
 
+async function getAll() {
+    return await Accessory.find({}).lean();
+};
+
 const accessoryService ={
     create,
+    getAll,
 };
 
 module.exports = accessoryService;
